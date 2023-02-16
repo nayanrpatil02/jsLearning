9170b2d1c22ab43bd2ad69d8499115379e676d4f
nayanrpatil02 <nayanrpatil02@gmail.com>
2023-02-16 19:06:01 +0530

    First JS assignment
    First JS assignment var

---
+++ 01_variable_assign.js
@@ -0,0 +1,25 @@
+//Step 1
+console.log("********** Step 1 **********");
+var myFullName;
+console.log("My Full Name is:",myFullName);
+var fullName = "Naayan Patil";
+console.log("My Full Name is:",fullName);
+var fullName = "Naayan Rajesh Patil";
+console.log("My Full Name is:",fullName);
+
+//Step 2
+console.log("********** Step 2 **********");
+var myLove = "I love only JS";
+console.log("myLove :",myLove);
+
+//Step 3
+console.log("********** Step 3 **********");
+var firstName = "Naayan";
+var lastName = "Patil";
+var age = "31";
+var address = "Pune";
+var pincode = "411028";
+var gender = "Female";
+var isMarried = "Single";
+
+console.log("My First Name is:",firstName, "My Last Name is:",lastName, "My age is",age, "my Address is",address, "My Pincode is",pincode, "My Gender is",gender,"My marital status is",isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,18 @@
+//Personal Details
 console.log("Naayan Patil");
+console.log(31);
+console.log("Pune");
+
+var fullName = "Naayan Patil";
+console.log(fullName);
+console.log(fullName);
+
+var city; //vaiable declaration
+console.log(city); 
+city="Pune"; //variable initialization
+console.log(city);
+city="Surat"; //variable update
+console.log(city);
+
+var age = "31";
+console.log(age);
