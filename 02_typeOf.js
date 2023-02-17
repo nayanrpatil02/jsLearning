280ec08fc04d5b41b54acdf6cceee12c6860a415
nayanrpatil02 <nayanrpatil02@gmail.com>
2023-02-17 13:36:33 +0530

    Variable and typeof Operator
    Assignment 2 on Variable and typeof Operator

---
+++ 02_typeOf.js
@@ -0,0 +1,30 @@
+console.log("******************** Step 1 ********************");
+console.log("----- BANK NAME -----");
+var bankName;
+bankName = "SBI Bank"
+console.log("Bank Name :",bankName, "Type Of Variable:", typeof bankName);
+
+console.log("                                                    ");
+console.log("******************** Step 2 ********************");
+console.log("----- MARKS -----");
+var marks = '90%';
+console.log("Marks",marks, "Type Of Variable:", typeof marks);
+
+console.log("                                                    ");
+console.log("******************** Step 3 ********************");
+console.log("----- WORKING STATUS -----");
+var isWorking;
+console.log("isWorking :",isWorking, "Type Of Variable :", typeof isWorking);
+isWorking = true;
+console.log("isWorking :",isWorking, "Type Of Variable :", typeof isWorking);
+
+console.log("                                                    ");
+console.log("******************** Step 4 ********************");
+console.log("----- TOTAL COUNT -----");
+totalCount = "Hundred and Seven";
+console.log("***** Step a *****");
+console.log("Total Count :",totalCount, "Type Of Variable :", typeof totalCount);
+console.log("***** Step b *****");
+totalCount = 107 ;
+console.log("Total Count :",totalCount, "Type Of Variable :", typeof totalCount);
+console.log("******************** END ********************");
