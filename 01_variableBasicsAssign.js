0e7df8ce9c4a5782b627df91f07fcff9379c1fb9
nayanrpatil02 <nayanrpatil02@gmail.com>
2023-02-17 12:59:33 +0530

    Variables and its datatypes
    Assignment 1 on basics of variables and their datatypes.
    Difference between NULL and undefined variable.

---
+++ 01_variableBasicsAssign.js
@@ -0,0 +1,31 @@
+console.log("******************** Step 1 ********************");
+console.log("                                                                                    ");
+console.log("_____ String Variable _____");
+
+var firstName = "Naayan";
+var middleName = "Rajesh";
+var lastName  = "Patil";
+console.log("First Name:",firstName, "Type Of Variable:", typeof firstName);
+console.log("Middle Name :",middleName, "Type Of Variable :", typeof middleName);
+console.log("Last Name :",lastName, "Type Of Variable :", typeof lastName);
+
+console.log("                                                                                    ");
+console.log("_____ Number Variable _____")
+var dateOfBirth = 02 ,birthYear =1992,age=31;
+console.log("Date Of Birth:",dateOfBirth, "Type Of Variable:", typeof dateOfBirth);
+console.log("Birth Year :",birthYear, "Type Of Variable :", typeof birthYear);
+console.log("Age:",age, "Type Of Variable :", typeof age);
+
+console.log("                                                                                    ");
+console.log("_____ Boolean Variable _____")
+var isMarried = false , isWorking = true, isFit = true;
+console.log("isMarried:",isMarried, "Type Of Variable:", typeof isMarried);
+console.log("isWorking :",isWorking, "Type Of Variable :", typeof isWorking);
+console.log("isFit:",isFit, "Type Of Variable :", typeof isFit);
+
+console.log("                                                                                    ");
+console.log("******************** Step 2 ********************");
+console.log("                                                                                    ");
+console.log("DIFFERENCE BETWEEN NULL AND UNDEFINED");
+console.log("NULL variable means the variable is declared and the value assigned is an empty value or no value ");
+console.log("and UNDEFINED means the variable is declared, but no value is assigned to it.");
---
+++ 02_datatypes.js
@@ -0,0 +1,36 @@
+var myName = "Virat Kohli";  // 'Virat Kohli' data type-string
+var fatherName = "Yogesh Kohli";
+
+var age = 31;   //data type- integer
+var weight = 82.5; 
+
+var isMarried = true; // datatype- boolean (true/false)
+var isLazy = false;
+
+var typeOfMyName = typeof myName;
+console.log("Type Of Variable myName :", typeOfMyName );
+
+console.log("Type Of Variable fatherName :", typeof fatherName);
+
+console.log("Type Of Variable Age :", typeof age);
+console.log("Type Of Variable Weight :", typeof weight);
+console.log("Type Of Variable Is Married :", typeof isMarried);
+console.log("Type Of Variable Is Lazy :", typeof isLazy);
+
+var graduation; //undefined value
+console.log("Value of Graduation variable is", graduation);
+console.log("Type Of Graduation is :", typeof graduation);
+
+var city = null; //null value
+console.log("Value of City variable is", city);
+console.log("Type Of City is :", typeof city);
+
+age = 32; //number
+age = "32"; //string
+age= "true" ; //in double quotes so string
+age = true; //boolean
+
+var alphabet = "A"; // alphabet='A' - string 
+
+console.log(typeof ("Virat Kohli"));
+console.log(typeof "Virat Kohli");
