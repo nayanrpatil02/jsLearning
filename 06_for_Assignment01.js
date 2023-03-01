8bfb21a533cafdcf0170b5efc9c020e235d613a4
nayanrpatil02 <nayanrpatil02@gmail.com>
2023-03-01 13:27:12 +0530

    For loop assignment 01
    For loop assignment 01

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,37 @@
+console.log("********** Program to print numbers from 5 to 15 by incrementing by 1 **********");
+for (let index = 5; index <= 15; index = index+1) {
+    console.log(index);
+ }
+
+ console.log("********** Program to print numbers from 50 to 40 by decrementing by 1 **********");
+ for (let index = 50 ; index >= 40; index = index-1) {
+    console.log(index);
+ }
+
+ console.log("********** Program to find first 15 odd numbers **********");
+for (let index = 0 ; index < 30; index = index+1) {
+    if (index%2==1) {
+    console.log(index);
+  }
+}
+console.log("********** Program to find first 10 even numbers **********");
+for (let index = 0 ; index < 20; index = index+1) {
+    if (index%2==0) {
+    console.log(index);
+  }
+}
+
+console.log("********** Program to print table of 5 **********");
+for (let index = 5; index <= 50 ; index = index+5) {
+    console.log(5 * index);
+ }
+
+ console.log("********** Program to print table of 10 **********");
+for (let index = 10; index <= 100 ; index = index+10) {
+    console.log(index);
+ }
+
+console.log("********** Program to print table of 10 in reverse order **********");
+ for (let index = 100 ; index >= 10 ; index = index-10) {
+     console.log(index);
+  }
---
+++ 06_for_loop.js
@@ -0,0 +1,30 @@
+//study of for loop
+
+//print numbers form 1 to 10
+for (let index = 1; index <= 10; index = index+1) {
+   console.log(index);
+}
+
+// print 0 to 5
+for (let index = 0; index <= 5; index = index+1) {
+    console.log(index);
+ }
+
+//print 10 to 0
+for (let index = 10; index >= 0; index=index-1) {
+    console.log(index);
+}
+
+//table of 5
+for (let index = 5; index <= 50 ; index = index+5) {
+    console.log(index);
+ }
+
+console.log("***********************************");
+/*for (let index = 1 ; index <= 0 ;) {  //index= 1  infinite loop
+    console.log(index);
+ }
+
+for (let index = 10 ; index >= 0 ; index++) {  //index= 10  infinite loop
+    console.log(index);
+ }*/
